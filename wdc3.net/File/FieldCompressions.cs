b2ee564b039a2708a4f413a312bdc2cbdd44e293
Michele Beckmann <m.beckmann@live.de>
2020-03-15 00:31:09 +0100

    added FieldCompressions enum

---
+++ wdc3.net/File/FieldCompressions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    public enum FieldCompressions
+    {
+        None,
+        Bitpacked,
+        CommonData,
+        BitpackedIndexed,
+        BitpackedIndexedArray,
+        BitpackedSigned
+    }
+}
