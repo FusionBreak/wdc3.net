3ca3af982f688cdc29f00cd6ac177490558d759c
Michele Beckmann <m.beckmann@live.de>
2020-03-15 00:21:04 +0100

    added Header and SectionHeader class

---
+++ wdc3.net/File/Header.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    public class Header
+    {
+        public int Magic { get; set; }
+        public int RecordCount { get; set; }
+        public int FieldCount { get; set; }
+        public int RecordSize { get; set; }
+        public int StringTableSize { get; set; }
+        public int TabeHash { get; set; }
+        public int LayoutHash { get; set; }
+        public int MinId { get; set; }
+        public int MaxId { get; set; }
+        public int Locale { get; set; }
+        public short Flags { get; set; }
+        public short IdIndex { get; set; }
+        public int TotalFieldCount { get; set; }
+        public int BitpackedDataOffset { get; set; }
+        public int LookUpColumnCount { get; set; }
+        public int FieldStorageInfoSize { get; set; }
+        public int CommonDataSize { get; set; }
+        public int PalletDataSize { get; set; }
+        public int SectionCount { get; set; }
+    }
+}
---
+++ wdc3.net/File/SectionHeader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    class SectionHeader
+    {
+        public long TactKeyHash { get; set; }
+        public int FileOffset { get; set; }
+        public int RecordCount { get; set; }
+        public int StringTableSize { get; set; }
+        public int OffsetRecordsEnd { get; set; }
+        public int IdListSize { get; set; }
+        public int RelationsshipDataSize { get; set; }
+        public int OffsetMapIdCount { get; set; }
+        public int CopyTableCount { get; set; }
+    }
+}
