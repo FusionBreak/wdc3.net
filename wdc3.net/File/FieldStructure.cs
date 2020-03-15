c809efd9e636d8e3b093989ead5d4863fc266395
Michele Beckmann <m.beckmann@live.de>
2020-03-15 01:49:47 +0100

    added FieldStructure

---
+++ wdc3.net/File/FieldStructure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    public class FieldStructure
+    {
+        public short Size { get; set; }
+        public short Position { get; set; }
+    }
+}
