b58651c94b87117af2f2ac9768a0f669b65c1299
Michele Beckmann <m.beckmann@live.de>
2020-03-24 11:21:20 +0100

    added RecordData

---
+++ wdc3.net/File/RecordData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    public class RecordData
+    {
+        public IEnumerable<byte> Data { get; set; }
+    }
+}
