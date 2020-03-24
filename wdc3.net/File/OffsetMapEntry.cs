47865f89496011ce5f6fd7cdbcfe96bebf48bbcc
Michele Beckmann <m.beckmann@live.de>
2020-03-24 11:24:58 +0100

    added OffsetMapEntry

---
+++ wdc3.net/File/OffsetMapEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    public class OffsetMapEntry
+    {
+        public int Offset { get; set; }
+        public byte Size { get; set; }
+    }
+}
