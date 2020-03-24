4fee38d06d7ad608ccc84e4388e74025917c990a
Michele Beckmann <m.beckmann@live.de>
2020-03-24 11:26:22 +0100

    added RelationshipEntry

---
+++ wdc3.net/RelationshipEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net
+{
+    public class RelationshipEntry
+    {
+        public int ForeignId { get; set; }
+        public int RecordIndex { get; set; }
+    }
+}
