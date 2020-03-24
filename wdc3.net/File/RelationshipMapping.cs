41be4969c78ca464635386fc94ec9e199f4d30a7
Michele Beckmann <m.beckmann@live.de>
2020-03-24 11:27:30 +0100

    added RelationshipMapping

---
+++ wdc3.net/File/RelationshipMapping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    public class RelationshipMapping
+    {
+        public int NumEntries { get; set; }
+        public int MinId { get; set; }
+        public int MaxId { get; set; }
+    }
+}
