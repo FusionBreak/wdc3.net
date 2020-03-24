e06816ffbe190d7806a0dff84f925f68fc19bb63
Michele Beckmann <m.beckmann@live.de>
2020-03-24 11:23:07 +0100

    added CopyTableEntry

---
+++ wdc3.net/File/CopyTableEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    public class CopyTableEntry
+    {
+        public int IdOfNewRow { get; set; }
+        public int IdOfCopiedRow { get; set; }
+    }
+}
