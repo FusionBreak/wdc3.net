bb20743bc6d27517ac36aef8ce1d9842c9123238
Michele Beckmann <m.beckmann@live.de>
2020-03-24 14:12:55 +0100

    added Db2

---
+++ wdc3.net/File/Db2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.File
+{
+    public class Db2
+    {
+        public Header Header { get; set; }
+        public IEnumerable<SectionHeader> SectionHeaders { get; set; }
+        public IEnumerable<FieldStructure> FieldStructures { get; set; }
+        public IEnumerable<IFieldStorageInfo> FieldStorageInfos { get; set; }
+        public IEnumerable<byte> PalletData { get; set; }
+        public IEnumerable<byte> CommonData { get; set; }
+        public IEnumerable<ISection> Sections { get; set; }
+    }
+}
