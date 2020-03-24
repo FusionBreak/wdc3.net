d24f0e54aecf07e8465e02c1a317352c131b9793
Michele Beckmann <m.beckmann@live.de>
2020-03-24 15:06:48 +0100

    created Db2Reader

---
+++ wdc3.net/Reader/Db2Reader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using wdc3.net.File;
+
+namespace wdc3.net.Reader
+{
+    public class Db2Reader
+    {
+        public Db2 ReadFile(string path)
+        {
+            var fileBytes = System.IO.File.ReadAllBytes(path);
+            Db2 db = new Db2();
+            //db.Header
+
+            return null;
+        }
+    }
+}
