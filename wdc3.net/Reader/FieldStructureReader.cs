b03064666e764d63f6afdce5ac12e7d7141b69bc
Michele Beckmann <m.beckmann@live.de>
2020-04-23 09:13:52 +0200

    added FieldStructureReader

---
+++ wdc3.net/Reader/FieldStructureReader.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using wdc3.net.File;
+
+namespace wdc3.net.Reader
+{
+    public class FieldStructureReader : IFileReader<IEnumerable<FieldStructure>>
+    {
+        BinaryReader _reader;
+        int _totalFieldCount;
+        public long Position { get; private set; }
+
+        public FieldStructureReader(BinaryReader reader, int totalFieldCount)
+        {
+            _reader = reader;
+            _totalFieldCount = totalFieldCount;
+        }
+
+        public IEnumerable<FieldStructure> Read()
+        {
+            var output = new List<FieldStructure>();
+
+            for (int currentStructureCount = 0; currentStructureCount < _totalFieldCount; currentStructureCount++)
+            {
+                var structure = new FieldStructure();
+
+                structure.Size = _reader.ReadInt16();
+                structure.Position = _reader.ReadUInt16();
+
+                output.Add(structure);
+            }
+
+            return output;
+        }
+    }
+}
