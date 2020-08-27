bc7cc6e27260efd919ecc006252d682ffb278c03
Michele Beckmann <m.beckmann@live.de>
2020-08-27 15:52:14 +0200

    DbdReader now reads the entire file and creates DataChunks

---
+++ wdc3.net.dbd/Enums/DataChunkNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.dbd.Enums
+{
+    internal static class DataChunkNames
+    {
+        public const string COLUMNS = "COLUMNS";
+        public const string LAYOUT = "LAYOUT";
+        public const string BUILD = "BUILD";
+        public const string COMMENT = "COMMENT";
+    }
+}
---
+++ wdc3.net.dbd/Reader/DbdReader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using wdc3.net.dbd.Enums;
+using wdc3.net.dbd.File;
+
+namespace wdc3.net.dbd.Reader
+{
+    public class DbdReader
+    {
+        public bool AllLinesReaded => _currentLineNumber >= _allLines?.Length;
+        private string[]? _allLines;
+        private int _currentLineNumber;
+        private string? CurrentLine => !AllLinesReaded ? _allLines?[_currentLineNumber] : "";     
+        private bool CurrentLineStartsWithDataChunk
+            => CurrentLine != null
+                && (
+                    CurrentLine.StartsWith(DataChunkNames.BUILD)
+                    || CurrentLine.StartsWith(DataChunkNames.COLUMNS)
+                    || CurrentLine.StartsWith(DataChunkNames.COMMENT)
+                    || CurrentLine.StartsWith(DataChunkNames.LAYOUT)
+                );
+
+        private List<DataChunk> _chunks = new List<DataChunk>();
+
+
+        public Db2Definition ReadFile(string path)
+        {
+            _allLines = System.IO.File.ReadAllLines(path);
+
+            if(CurrentLine == null)
+                throw new Exception("Empty line.");
+
+            while(!AllLinesReaded)
+            {
+                if(CurrentLineStartsWithDataChunk)
+                {
+                    var header = readChunkHeader();
+                    var chunk = new DataChunk
+                    {
+                        Name = header.Name,
+                        Parameters = header.Parameters,
+                        Content = readChunkContent().ToList()
+                    };
+                    _chunks.Add(chunk);
+                }
+                else
+                {
+                    _currentLineNumber++;
+                }
+            }
+            
+            
+            return null;
+        }
+
+
+
+        private (string? Name, IEnumerable<string>? Parameters) readChunkHeader()
+        {
+            var output = (
+                CurrentLine?.Split(' ')[0],
+                CurrentLine?.Split(' ')[1..]
+                            .Select(content => content.Trim().Replace(",", ""))
+            );
+
+            _currentLineNumber++;
+
+            return output;
+        }
+
+        private IEnumerable<string> readChunkContent()
+        {
+            while(CurrentLine != null && CurrentLine != "")
+            {
+                yield return CurrentLine;
+                _currentLineNumber++;
+            }
+        }
+    }
+}
