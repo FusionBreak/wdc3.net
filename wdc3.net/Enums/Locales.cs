3bb829457e5e493cf0ffe656124a58b7727c71df
Michele Beckmann <m.beckmann@live.de>
2020-08-30 22:40:33 +0200

    added Locales enum

---
+++ wdc3.net/Enums/Locales.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wdc3.net.Enums
+{
+    public enum Locales
+    {
+        None = -1,
+        EnUS = 0,
+        EnGB = EnUS,
+        KoKR = 1,
+        FrFR = 2,
+        DeDE = 3,
+        EnCN = 4,
+        ZhCN = EnCN,
+        EnTW = 5,
+        ZhTW = EnTW,
+        EsES = 6,
+        EsMX = 7,
+        RuRU = 8,
+        PtPT = 10,
+        PtBR = PtPT,
+        ItIT = 11,
+    }
+}
