cf686b3e067b1586990ca89bb8a31367323ee141
Michele Beckmann <m.beckmann@live.de>
2020-08-30 17:01:17 +0200

    BuildInfo is now compared by its content

--- wdc3.net.dbd/File/BuildInfo.cs
+++ wdc3.net.dbd/File/BuildInfo.cs
@@ -10,5 +10,13 @@ namespace wdc3.net.dbd.File
         public int Major { get; set; }
         public int Minor { get; set; }
         public int Build { get; set; }
+
+        public override bool Equals(object? obj) 
+            => obj is BuildInfo build
+                && build != null 
+                && build.Expansion == Expansion
+                && build.Major == Major
+                && build.Minor == Minor
+                && build.Build == Build;
     }
 }
