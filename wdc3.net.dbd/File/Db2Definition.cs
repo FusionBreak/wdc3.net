9f4e1104cd99362882763ce17175f64b91e88be8
Michele Beckmann <m.beckmann@live.de>
2020-08-30 17:02:01 +0200

    added GetVersionDefinition(string buildString) to Db2Definition

--- wdc3.net.dbd/File/Db2Definition.cs
+++ wdc3.net.dbd/File/Db2Definition.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using wdc3.net.dbd.Factorys;
 
 namespace wdc3.net.dbd.File
 {
@@ -9,5 +11,10 @@ namespace wdc3.net.dbd.File
         public IEnumerable<ColumnDefinition>? ColumnDefinitions { get; set; }
         public IEnumerable<VersionDefinition>? VersionDefinitions { get; set; }
         public string? Comment { get; set; }
+
+        public VersionDefinition GetVersionDefinition(string buildString)
+            => VersionDefinitions
+                .Where(version => version.Builds.Contains(new BuildInfoFactory().CreateFromBuildString(buildString)))
+                .First();
     }
 }
