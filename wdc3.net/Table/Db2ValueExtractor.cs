17025ed12ae60fb9f405bb395b789255195548b3
Michele Beckmann <m.beckmann@live.de>
2020-11-22 17:48:16 +0100

    add FieldCompressions.None reading

--- wdc3.net/Table/Db2ValueExtractor.cs
+++ wdc3.net/Table/Db2ValueExtractor.cs
@@ -15,8 +15,8 @@ namespace wdc3.net.Table
         private IEnumerable<byte> _commonData;
         private IEnumerable<byte> _recordData;
         private IEnumerable<byte> _recordStringData;
-        private IEnumerable<byte> __recordDataCombined => _recordData.Concat(_recordStringData);
-        //private int _recordDataPosition = 0;
+        private IEnumerable<byte> _recordDataCombined => _recordData.Concat(_recordStringData);
+        private int _recordDataPosition = 0;
 
         private IEnumerable<int> _palletValues
         {
@@ -43,43 +43,62 @@ namespace wdc3.net.Table
             switch(fieldStorageInfo.StorageType)
             {
                 case FieldCompressions.None:
-                    var fieldOffset = fieldStorageInfo.FieldOffsetBits / 8;
                     var size = fieldStorageInfo.FieldSizeBits / 8;
-                    var value = readInt(fieldOffset, size);
-                    //_recordDataPosition += fieldStorageInfo.FieldSizeBits / 8;
-                    return type == typeof(string) ? readString(value+fieldStructure.Position) : value;
-
-                //case FieldCompressions.Bitpacked:
-                //    return null;
+                    var offsetByteCount = fieldStorageInfo.FieldOffsetBits / 8;
+                    var fieldOffset = _recordDataPosition;
+                    var value = readInt(_recordDataPosition, size);
+                    return type == typeof(string) ? readString(value + fieldStructure.Position) : value; //$"{value} | {fieldStructure.Position}"; //
+                // Bitpacked -- the field is a bitpacked integer in the record data.  It
+                // is field_size_bits long and starts at field_offset_bits.
+                // A bitpacked value occupies
+                //   (field_size_bits + (field_offset_bits & 7) + 7) / 8
+                // bytes starting at byte
+                //   field_offset_bits / 8
+                // in the record data.  These bytes should be read as a little-endian value,
+                // then the value is shifted to the right by (field_offset_bits & 7) and
+                // masked with ((1ull << field_size_bits) - 1).
+                case FieldCompressions.Bitpacked:
+                case FieldCompressions.BitpackedSigned:
+                    //var a_size = (fieldStorageInfo.FieldSizeBits + (fieldStorageInfo.FieldOffsetBits & 7) + 7) / 8;
+                    //var a_offset = fieldStorageInfo.FieldOffsetBits / 8;
+                    //var a_value = readByte(_recordDataPosition);
+                    return readByte(_recordDataPosition);
                 //case FieldCompressions.CommonData:
                 //    return null;
                 case FieldCompressions.BitpackedIndexed:
-                    var index = _recordData.Skip(fieldStorageInfo.FieldOffsetBits / 8).First();
+                    var index = readByte(_recordDataPosition);
                     return _palletValues.Skip(index).First();
                 //case FieldCompressions.BitpackedIndexedArray:
-                //    return null;
-                //case FieldCompressions.BitpackedSigned:
                 //    return null;
                 default:
                     return fieldStorageInfo.StorageType;
             }
         }
 
+        private byte readByte(int offset)
+        {
+            var value = _recordData.Skip(_recordDataPosition).First();
+            _recordDataPosition++;
+            return value;
+        }
+
         private int readInt(int offset, int size)
         {
             var value = BitConverter.ToInt32(_recordData.Skip(offset).Take(size).ToArray());
+            _recordDataPosition += size;
             return value;
         }
 
         private string readString(int offset)
         {
-            var chars = __recordDataCombined
+            if(offset - _recordData.Count() < 0)
+                return $"ERROR:{offset - _recordData.Count()} ";
+
+            var chars = _recordDataCombined
                         .Skip(offset)
                         .TakeWhile(x => x != 0)
                         .Select(x => Convert.ToChar(x))
                         .ToArray();
-
-            var x = offset - _recordData.Count();
 
             return new string(chars);
         }
