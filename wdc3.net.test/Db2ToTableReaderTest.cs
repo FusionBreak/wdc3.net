5138778566bfd383df130535d74f04a59f88ff85
Michele Beckmann <m.beckmann@live.de>
2021-01-12 13:48:19 +0100

    added more testfiles

--- wdc3.net.test/Db2ToTableReaderTest.cs
+++ wdc3.net.test/Db2ToTableReaderTest.cs
@@ -5,11 +5,51 @@ namespace wdc3.net.test
     public class Db2ToTableReaderTest
     {
         [Fact]
-        public void XXX()
+        public void ReadsMap()
         {
             //Compare with https://wow.tools/dbc/?dbc=map&build=8.3.7.34872#page=1
 
             var reader = new Db2ToTableReader(TestFiles.MAP_DB2_PATH, TestFiles.MAP_DBD_PATH);
+            var table = reader.Read();
+            _ = table;
+        }
+
+        [Fact]
+        public void ReadsMap902()
+        {
+            var reader = new Db2ToTableReader(TestFiles.MAP_9_DB2_PATH, TestFiles.MAP_DBD_PATH);
+            var table = reader.Read();
+            _ = table;
+        }
+
+        [Fact]
+        public void ReadsItem()
+        {
+            var reader = new Db2ToTableReader(TestFiles.ITEM_DB2_PATH, TestFiles.ITEM_DBD_PATH);
+            var table = reader.Read();
+            _ = table;
+        }
+
+        [Fact]
+        public void ReadsItemSparse()
+        {
+            var reader = new Db2ToTableReader(TestFiles.ITEM_SPARSE_DB2_PATH, TestFiles.ITEM_SPARSE_DBD_PATH);
+            var table = reader.Read();
+            _ = table;
+        }
+
+        [Fact]
+        public void ReadsLocale()
+        {
+            var reader = new Db2ToTableReader(TestFiles.LOCALE_DB2_PATH, TestFiles.LOCALE_DBD_PATH);
+            var table = reader.Read();
+            _ = table;
+        }
+
+        [Fact]
+        public void ReadsTextureFileData()
+        {
+            var reader = new Db2ToTableReader(TestFiles.TEXTURE_FILE_DATA_DB2_PATH, TestFiles.TEXTURE_FILE_DATA_DBD_PATH);
             var table = reader.Read();
             _ = table;
         }
--- wdc3.net.test/TestFiles.cs
+++ wdc3.net.test/TestFiles.cs
@@ -10,5 +10,14 @@ namespace wdc3.net.test
     {
         public const string MAP_DB2_PATH = "./../../../TestFiles/map.db2";
         public const string MAP_DBD_PATH = "./../../../TestFiles/Map.dbd";
+        public const string MAP_9_DB2_PATH = "./../../../TestFiles/map_9_0_2.db2";
+        public const string ITEM_DB2_PATH = "./../../../TestFiles/item.db2";
+        public const string ITEM_DBD_PATH = "./../../../TestFiles/Item.dbd";
+        public const string ITEM_SPARSE_DB2_PATH = "./../../../TestFiles/item-sparse.db2";
+        public const string ITEM_SPARSE_DBD_PATH = "./../../../TestFiles/ItemSparse.dbd";
+        public const string LOCALE_DB2_PATH = "./../../../TestFiles/locale.db2";
+        public const string LOCALE_DBD_PATH = "./../../../TestFiles/Locale.dbd";
+        public const string TEXTURE_FILE_DATA_DB2_PATH = "./../../../TestFiles/texturefiledata.db2";
+        public const string TEXTURE_FILE_DATA_DBD_PATH = "./../../../TestFiles/TextureFileData.dbd";
     }
 }
